19e5cb6fcee9afd27a692e306577dfd0c190c44e
vini00784 <vo70346@gmail.com>
2024-05-02 09:45:52 -0300

    fix: format code

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -3,7 +3,7 @@ import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
 
 @Module({
-    controllers: [TaskController],
-    providers: [TaskService]
+  controllers: [TaskController],
+  providers: [TaskService],
 })
 export class TaskModule {}
