e984d2483931d62b5b8b83e663a0c8d943266fb7
vini00784 <vo70346@gmail.com>
2024-05-03 18:31:14 -0300

    feat: service to login route

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import { AuthResponseDto } from './auth.dto';
+import { compareSync as bcryptCompareSync } from 'bcrypt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AuthService {
+    private jwtExpirationInSeconds: number;
+
+    constructor(
+        private readonly userService: UsersService,
+        private readonly jwtService: JwtService,
+        private readonly configService: ConfigService
+    ) {
+        this.jwtExpirationInSeconds = +this.configService.get<number>('JWT_EXPIRATION_TIME')
+    }
+
+    signIn(username: string, password: string): AuthResponseDto {
+        const foundUser = this.userService.findByUsername(username);
+
+        if(!foundUser || !bcryptCompareSync(password, foundUser.password)) {
+            throw new UnauthorizedException();
+        }
+
+        const payload = { sub: foundUser.id, username: foundUser.username };
+
+        const token = this.jwtService.sign(payload);
+
+        return {
+            token,
+            expiresIn: this.jwtExpirationInSeconds
+        }
+    }
+}
