ba21f5963b4a388e1c1bdc73ac69062e8ec7a66d
vini00784 <vo70346@gmail.com>
2024-05-03 18:45:53 -0300

    feat: import config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TaskModule, UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ // O "forRoot" é usado para configurar o módulo de configuração
+      isGlobal: true // O "isGlobal" indica que as configurações definidas pelo "ConfigModule" devem ser globais, o que significa que elas podem ser acessadas de qualquer lugar no aplicativo. Isso é útil para configurações que são compartilhadas por vários módulos ou serviços em um aplicativo NestJS
+    }),
+    TaskModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
