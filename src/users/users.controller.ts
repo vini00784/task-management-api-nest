bc4ed1cda42f1601d4599274013b5fdfacad6720
vini00784 <vo70346@gmail.com>
2024-05-03 09:01:41 -0300

    feat: user settings

---
+++ src/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UserDto } from './user.dto';
+
+@Controller('users')
+export class UsersController {
+    constructor(private readonly userService: UsersService) {}
+
+    @Post()
+    createUser(@Body() userInfo: UserDto) {
+        this.userService.createUser(userInfo);
+    }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { UserDto } from './user.dto';
+import { randomUUID } from 'node:crypto';
+import { hashSync as bcryptHashSync } from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+    private readonly users: UserDto[] = []
+
+    createUser(newUser: UserDto) {
+        newUser.id = randomUUID();
+        newUser.password = bcryptHashSync(newUser.password, 10);
+        this.users.push(newUser);
+        console.log(this.users);
+    }
+}
