e94168b3055f20602c0458803412e6e214683993
vini00784 <vo70346@gmail.com>
2024-05-03 09:40:40 -0300

    feat: import auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TaskModule, UsersModule],
+  imports: [TaskModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
