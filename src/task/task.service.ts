7af75257603217a033f6019ef3230f4b60badced
vini00784 <vo70346@gmail.com>
2024-05-13 08:22:11 -0300

    feat: set task status and ID

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -4,15 +4,17 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
-import { GetAllParameters, TaskDto } from './task.dto';
+import { GetAllParameters, TaskDto, TaskStatusEnum } from './task.dto';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class TaskService {
   private tasks: TaskDto[] = [];
 
   createTask(task: TaskDto) {
+    task.id = uuid();            
+    task.status = TaskStatusEnum.TO_DO;
     this.tasks.push(task);
-    console.log(this.tasks);
   }
 
   getTaskById(id: string): TaskDto {
